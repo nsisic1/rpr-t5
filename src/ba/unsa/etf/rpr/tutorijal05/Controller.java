b6c713261fac31f15de3c5e7c893ea5412c743eb
Nedim Sisic <nsisic1@etf.unsa.ba>
2018-11-14 23:30:14 +0100

    Dodana polja i enkapsulacija kontrolera

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,43 @@
 package ba.unsa.etf.rpr.tutorijal05;
+import javafx.scene.text.Text;
 
 public class Controller {
+
+    private Text tekst;
+    private double broj = 0;
+    private String operacija = "";
+
+    public Controller() {
+        tekst = new Text("0");
+    }
+
+    public String getTekst() {
+        return tekst.getText();
+    }
+
+    public void setTekst(String s) {
+        this.tekst.setText(s);
+    }
+
+    public Text tekstProperty() {
+        return tekst;
+    }
+
+    public double getbroj() {
+        return broj;
+    }
+
+    public void setbroj(double broj) {
+        this.broj = broj;
+    }
+
+    public String getoperacija() {
+        return operacija;
+    }
+
+    public void setoperacija(String operacija) {
+        this.operacija = operacija;
+    }
+
 
 }
